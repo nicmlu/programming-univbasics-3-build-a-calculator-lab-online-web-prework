93b86c628f60deb0afb2a9486c96082cec81beb5
Nicole Lucien <nicolemlucien@gmail.com>
2019-09-22 23:33:01 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+p sum
 end
 
 def subtraction(num1, num2)
-
+  difference = num2 - num1
+p difference
 end
 
 def division(num1, num2)
-
+  quotient = num2 / num1
+p quotient
 end
 
 def multiplication(num1, num2)
-
+  multiply = num1 * num2
+p multiply
 end
 
 def modulo(num1, num2)
